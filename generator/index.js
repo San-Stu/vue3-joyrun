dee020aa2175cb2e2a7e18675f13a2fad0cc8f0d
sanstu <13580328628@163.com>
2020-12-30 17:08:15 +0800

    update

--- generator/index.js
+++ generator/index.js
@@ -1,5 +1,8 @@
 module.exports = (api, options, rootOptions) => {
-
+  // 修改package.json
+  api.extendPackage({
+    'name': options.name 
+  })
   // 复制并用 ejs 渲染 `./template` 内所有的文件
   api.render('../template')
 }
